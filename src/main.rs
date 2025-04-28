20bb4bf692adc823ba70263c129373c9ce7058f4
Jason Guo <jasonguo@Jasons-MacBook-Pro.local>
2025-04-28 13:30:20 -0700

    added constant folding

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,17 @@ mod prospero;
 fn main() {
     let input = "/Users/jasonguo/egg-prospero/prospero.vm";
     let start = prospero::parse_lang_expr(input).unwrap();
-    //println!("Final expression: {}", start);
+    //let start: RecExpr<prospero::Prospero> = "(max (neg (+ 10 5)) 1)".parse().unwrap();
+    //println!("Starting expression: {}", start);
 
-    let rules: &[Rewrite<prospero::Prospero, ()>] = &[
+    let rules: &[Rewrite<prospero::Prospero, prospero::ConstantFold>] = &[
         rw!("commute-add"; "(+ ?x ?y)" => "(+ ?y ?x)"),
         ];
-    let runner = Runner::default().with_expr(&start).run(prospero::rules());
+    let runner: Runner<prospero::Prospero, prospero::ConstantFold> = 
+        Runner::default().with_expr(&start).run(rules);
     let extractor = Extractor::new(&runner.egraph, AstSize);
     let (best_cost, best_expr) = extractor.find_best(runner.roots[0]);
+    println!("{}", best_expr);
 }
 
 
--- src/prospero.rs
+++ src/prospero.rs
@@ -5,6 +5,7 @@ pub type Constant = NotNan<f64>;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
+pub type EGraph = egg::EGraph<Prospero, ConstantFold>;
 pub type Rewrite = egg::Rewrite<Prospero, ()>;
 
 
@@ -15,13 +16,88 @@ define_language! {
         "+" = Add([Id; 2]),
         "-" = Sub([Id; 2]),
         "*" = Mul([Id; 2]),
-        "max" = Test([Id; 2]),
         "min" = Min([Id; 2]),
         "max" = Max([Id; 2]),
         "neg" = Neg(Id),
         "square" = Square(Id),
         "sqrt" = Sqrt(Id),
         Constant(Constant),
+    }
+}
+
+
+#[derive(Default)]
+pub struct ConstantFold;
+impl Analysis<Prospero> for ConstantFold {
+    type Data = Option<(Constant, PatternAst<Prospero>)>;
+
+    fn make(egraph: &mut EGraph, enode: &Prospero) -> Self::Data {
+        let x = |i: &Id| egraph[*i].data.as_ref().map(|d| d.0);
+        Some(match enode {
+            Prospero::Constant(c) => (*c, format!("{}", c).parse().unwrap()),
+            Prospero::Add([a, b]) => (
+                x(a)? + x(b)?,
+                format!("(+ {} {})", x(a)?, x(b)?).parse().unwrap(),
+            ),
+            Prospero::Sub([a, b]) => (
+                x(a)? - x(b)?,
+                format!("(- {} {})", x(a)?, x(b)?).parse().unwrap(),
+            ),
+            Prospero::Mul([a, b]) => (
+                x(a)? * x(b)?,
+                format!("(* {} {})", x(a)?, x(b)?).parse().unwrap(),
+            ),
+            Prospero::Square(a) => (
+                x(a)? * x(a)?,
+                format!("(square {})", x(a)?).parse().unwrap(),
+            ),
+            Prospero::Neg(a) => (
+                -x(a)?,
+                format!("(neg {})", x(a)?).parse().unwrap(),
+            ),
+            Prospero::Sqrt(a) => (
+                NotNan::<f64>::new(x(a)?.sqrt()).unwrap(),
+                format!("(sqrt {})", x(a)?).parse().unwrap(),
+            ),
+            Prospero::Max([a, b]) => (
+                x(a)?.max(x(b)?),
+                format!("(max {} {})", x(a)?, x(b)?).parse().unwrap(),
+            ),
+            Prospero::Min([a, b]) => (
+                x(a)?.min(x(b)?),
+                format!("(min {} {})", x(a)?, x(b)?).parse().unwrap(),
+            ),
+            _ => return None,
+        })
+    }
+
+    fn merge(&mut self, to: &mut Self::Data, from: Self::Data) -> DidMerge {
+        merge_option(to, from, |a, b| {
+            assert_eq!(a.0, b.0, "Merged non-equal constants");
+            DidMerge(false, false)
+        })
+    }
+
+    fn modify(egraph: &mut EGraph, id: Id) {
+        let data = egraph[id].data.clone();
+        if let Some((c, pat)) = data {
+            if egraph.are_explanations_enabled() {
+                egraph.union_instantiations(
+                    &pat,
+                    &format!("{}", c).parse().unwrap(),
+                    &Default::default(),
+                    "constant_fold".to_string(),
+                );
+            } else {
+                let added = egraph.add(Prospero::Constant(c));
+                egraph.union(id, added);
+            }
+            // to not prune, comment this out
+            egraph[id].nodes.retain(|n| n.is_leaf());
+
+            #[cfg(debug_assertions)]
+            egraph[id].assert_unique_leaves();
+        }
     }
 }
 
@@ -64,7 +140,7 @@ pub fn parse_lang_expr(input: &str) -> io::Result<RecExpr<Prospero>> {
         list.push(node);
         ids.insert(id.to_string(), index.into());
     }
-    println!("{:?}", list);
+    //println!("{:?}", list);
     return Ok(egg::RecExpr::<Prospero>::from(list));
 }
 
